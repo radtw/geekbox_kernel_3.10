03b79ff0cbcd1e0930571540345d216bc28e9033
radish_r <radish_r@hotmail.com>
2019-07-14 12:42:42 +0100

    geekbox 3.10 kernel - allowing boot from USB/SATA HD

---
+++ drivers/gator/gator_main2.c
@@ -0,0 +1,1 @@
+gator_main.c
---
+++ include/tsai_macro.h
@@ -0,0 +1,28 @@
+/*
+ * sruk_bkpt.h
+ *
+ *  Created on: 7 Nov 2016
+ *      Author: cheng.tsai
+ */
+
+#ifndef TSAI_ASSERT_H_
+#define TSAI_ASSERT_H_
+
+#if defined(__aarch64__)
+	#define BKPT __asm("hlt #0")
+#else
+	#define BKPT __asm("bkpt")
+#endif
+
+#if !defined(ASSERT)
+	#if defined(DEBUG)
+		#define ASSERT(x) if (!(x)) {BKPT;}
+	#else
+		#define ASSERT(x)
+	#endif
+#endif
+
+extern int tsai_move_on; /* instance in tsai_spy.c */
+#define TSAI_BUSY_WAIT while(!tsai_move_on) cpu_relax();
+
+#endif /* TSAI_ASSERT_H_ */
